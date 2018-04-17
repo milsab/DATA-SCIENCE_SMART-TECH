04bb67af0d1104b73daafade71cea8c2f5e168dc
milsab <milad.sabouri@gmail.com>
2018-04-17 18:44:08 -0500

    add codes and edit readme file

--- itmd-521/Week-13/ParseData.py
+++ itmd-521/Week-13/ParseData.py
@@ -1,60 +1,70 @@
-# import mysql.connector
 
-# db = mysql.connector.connect(host="localhost",
-#                      user="root",
-#                      passwd="itmd521",
-#                      db="itmd521")
-#
-# cur = db.cursor()
-#
-# command = ("INSERT INTO records "
-#                "(us_station, wb_station, date, hour, latitude, longitude, kind, elevation, wind, quality, visibility, temperature, dew, pressure) "
-#                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
-
-with open("1985.txt", encoding='windows-1252') as file:
+with open("1985.txt", encoding='windows-1252') as readFile, open("dataset.txt", "w") as writeFile:
     i = 0
-    for line in file:
+    for line in readFile:
         i = i + 1
-        print(i)
-        us_station = line[4:10]
-        wb_station = line[10:15]
-        date = line[15:23]
-        hour = line[23:27]
-        latitude = line[28:34]
+        if i % 1000000 == 0:
+            print(i)
         longitude = line[34:41]
-        kind = line[41:46]
-        elevation = line[46:51]
-        wind = line[60:63]
-        quality = line[63:64]
-        visibility = line[78:84]
+        sign = line[34:35]
+        f_lon = line[36:38]
+        int_lon = int(f_lon)
+        if int_lon == 0:
+            fnal_lon = "0"
+        if int_lon == 1:
+            fnal_lon = sign + "10"
+        if int_lon == 2:
+            fnal_lon = sign + "20"
+        if int_lon == 3:
+            fnal_lon = sign + "30"
+        if int_lon == 4:
+            fnal_lon = sign + "40"
+        if int_lon == 5:
+            fnal_lon = sign + "50"
+        if int_lon == 6:
+            fnal_lon = sign + "60"
+        if int_lon == 7:
+            fnal_lon = sign + "70"
+        if int_lon == 8:
+            fnal_lon = sign + "80"
+        if int_lon == 9:
+            fnal_lon = sign + "90"
+        if int_lon == 10:
+            fnal_lon = sign + "100"
+        if int_lon == 11:
+            fnal_lon = sign + "110"
+        if int_lon == 12:
+            fnal_lon = sign + "120"
+        if int_lon == 13:
+            fnal_lon = sign + "130"
+        if int_lon == 14:
+            fnal_lon = sign + "140"
+        if int_lon == 15:
+            fnal_lon = sign + "150"
+        if int_lon == 16:
+            fnal_lon = sign + "160"
+        if int_lon == 17:
+            fnal_lon = sign + "170"
+        if int_lon == 18:
+            fnal_lon = sign + "180"
         # handle the + sign in temperature
         if line[87] == '+':
             temperature = line[88:92]
         else:
             temperature = line[87:92]
-        dewPoint = line[93:98]
-        atmosphericPressure = line[99:104]
-        data = (us_station, wb_station, date, hour, latitude, longitude, kind, elevation, wind, quality, visibility, temperature, dewPoint, atmosphericPressure)
+        quality = line[63:64]
+        data = [fnal_lon, quality, temperature]
 
         try:
-            # cur.execute(command, data)
-            file.write('\t'.join(data[1:]) + '\n')
+            writeFile.write('\t'.join(data) + '\n')
         except:
             print("Error to write data")
-        # every 100000 insert commit to the database
-        # if i % 100000 == 0:
-        #     try:
-        #         db.commit()
-        #     except:
-        #         print("Error to commit")
-        if i == 10:
-            break
+        # if i == 10000:
+        #     break
+    print(i)
 try:
-    # db.commit()
-    file.close()
+    readFile.close()
+    writeFile.close()
     print("All Data Successfully wrote to the file!")
 except:
     print("error to final wrote")
-
-# cur.close()
-# db.close()
--- itmd-521/Week-13/README.md
+++ itmd-521/Week-13/README.md
@@ -34,3 +34,16 @@ Submit your Github repo URL to blackboard by 11:59 pm April 19th.
 In your Week-13 folder include all scripts and program instructions needed to retrieve and reproduce your results.  Upload your part-r-0000 file (results) as well)  Write and instruction/assumptions needed in the Assumption section below.
 
 ### Assumptions 1
+
+
+
+### Deliverable 1
+The below is a screens hot of the out for part-00000. Also, I have uploaded the actual part-00000 file to this repository.
+![output](images/output_part0.png "part-00000 file")
+
+### Assumptions 1
+
+* First I used python to read the txt file and extract the longitude, air quality, and temperature to put them to a new text file. Also, I did the necessary data preprocessing and data filtration in this step.
+* Then, I used the python to run the job in Spark
+* I applied spark-submit --master local /vagrant/HW13/MaxTemperature.py dataset.txt output/ command to run the job in Spark.
+* In the output, each row represents the 10 degree of longitude. For example, (60, 592) means that the max temperature in longitude degree between 60.000 and 60.9999 is 59.2 Celsius.
