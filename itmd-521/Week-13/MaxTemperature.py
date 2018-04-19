e77720b5b2c47ecce896026c0306cf1a7a16c820
milsab <milad.sabouri@gmail.com>
2018-04-19 17:11:48 -0500

    edit small copy mistake

--- itmd-521/Week-13/MaxTemperature.py
+++ itmd-521/Week-13/MaxTemperature.py
@@ -5,7 +5,7 @@ import re, sys
 sc = SparkContext("local", "Max Temperature")
 sc.textFile(sys.argv[1]) \
   .map(lambda s: s.split("\t")) \
-  .filter(lambda rec: (int(rec[0]) >= 0 and int(rec[0]) <= 10000 and rec[2] != "9999" and re.match("[01459]", rec[1]))) \
+  .filter(lambda rec: ( rec[2] != "9999" and re.match("[01459]", rec[1]))) \
   .map(lambda rec: (int(rec[0]), int(rec[2]))) \
   .reduceByKey(max) \
   .saveAsTextFile(sys.argv[2])
